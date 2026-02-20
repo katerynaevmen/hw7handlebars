ff654c569804a7604fbd027f25ffc88104aa31d8
katerynaevmen <evmenetskaterina@gmail.com>
2026-02-20 12:29:59 +0200

    fixing files

--- src/app.js
+++ src/app.js
@@ -2,4 +2,4 @@ import {products} from "./data.js";
 import templatedata from "./template-data.hbs";
 
 const listRef = document.querySelector(".list");
-listRef.innerHTML = templatedata({products});
+listRef.innerHTML = templatedata({products})
