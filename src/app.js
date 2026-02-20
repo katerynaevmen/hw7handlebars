4901ec7c06662536f7681d3ff556cf01a25d3e8a
katerynaevmen <evmenetskaterina@gmail.com>
2026-02-20 11:48:14 +0200

    adding files

---
+++ src/app.js
@@ -0,0 +1,5 @@
+import {products} from "./data.js";
+import templatedata from "./template-data.hbs";
+
+const listRef = document.querySelector(".list");
+listRef.innerHTML = templatedata({products});
